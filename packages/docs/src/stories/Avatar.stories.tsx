fec007a5257fbdb4814046eebc4e399fd1a90a19
Victor Bruno <victorbrunof@icloud.com>
2022-10-30 14:59:19 -0300

    Melhorias no Storybook - Controles nos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/victorbrunof.png',
     alt: 'Victor Bruno',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -4,12 +4,15 @@ import { Box, BoxProps, Text } from '@ignite-ui/react'
 export default {
   title: 'Surfaces/Box',
   component: Box,
-  args: {
-    children: (
-      <>
-        <Text>Testando o elemento Box</Text>
-      </>
-    ),
+    args: {
+    children: <Text>Testando o elemento Box</Text>,
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
   },
 } as Meta<BoxProps>
 
